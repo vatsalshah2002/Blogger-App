eb46b1f90b616f4ec768fce75585a07604ce1524
vatsalshah2002 <74639313+vatsalshah2002@users.noreply.github.com>
2024-10-05 00:04:06 +0530

    Update postApi.js

--- Frontend/APIServices/posts/postApi.js
+++ Frontend/APIServices/posts/postApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5000/api/v1/posts"; //!Create post api
+//const BASE_URL = "http://localhost:5000/api/v1/posts"; //!Create post api
+const BASE_URL = "https://blogger-app-api.vercel.app/"
 export const createPostAPI = async (postData) => {
 
     const response = await axios.post(`${BASE_URL}/create`, {
